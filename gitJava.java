3e0f161a887a76fee799337fcad7121468407a2d
Nann Koungkea <161784844+Koungkea101@users.noreply.github.com>
2024-03-06 20:15:15 +0700

    testing Change

---
+++ gitJava.java
@@ -0,0 +1,27 @@
+public class gitJava {
+
+	public static void main(String[] args) {
+		System.out.println("    0|    1|    2|    3|    4|    5|    6|    7|    8|    9|    10|");
+		System.out.println("-------------------------------------------------------------------");
+		System.out.println("    1|    1|    2|    3|    4|    5|    6|    7|    8|    9|    10|");
+		System.out.println("    2|    2|    4|    6|    8|   10|   12|   14|   16|   18|    20|");
+		System.out.println("    3|    3|    6|    9|   12|   15|   18|   21|   24|   27|    30|");
+		System.out.println("    4|    4|    8|   12|   16|   20|   24|   28|   32|   36|    40|");
+		System.out.println("    5|    5|   10|   15|   20|   25|   30|   35|   40|   45|    50|");
+		System.out.println("\n\n");
+		sskjdf
+		System.out.println("+----------------+---------+-------------------+------+");
+		System.out.println("| Country        | Name    | Profession        | Age  |");
+		System.out.println("+----------------+---------+-------------------+------+");
+		System.out.println("| Germany        | Michael | Computer Engineer | 19   |");
+		System.out.println("+----------------+---------+-------------------+------+");
+		System.out.println("| England        | Robert  | Artist            | 34   |");
+		System.out.println("+----------------+---------+-------------------+------+");
+		System.out.println("| United Kingdom | Julia   | Designer          | 42   |");
+		System.out.println("+----------------+---------+-------------------+------+");
+		System.out.println("| United States  | Jo      | Actor             | 21   |");
+		System.out.println("+----------------+---------+-------------------+------+");
+
+	}
+
+}
